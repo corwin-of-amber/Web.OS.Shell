633b3c1f7ea85d937e45e4873302e290f125bbe1
Shachar Itzhaky <corwin.amber@gmail.com>
2019-12-28 15:14:28 +0200

    [build] Parcel-friendliness.

--- src/terminal.ts
+++ src/terminal.ts
@@ -1,5 +1,5 @@
 // build with
-// parcel watch --hmr-hostname=localhost src/terminal.ts
+// parcel watch --hmr-hostname=localhost --public-url '.' src/index.html &
 import { EventEmitter } from 'events';
 import $ from 'jquery';
 import { Terminal } from 'xterm';
@@ -9,6 +9,7 @@ import { WorkerPool, ProcessLoader } from 'wasi-kernel/src/kernel/services/worke
 import { SharedVolume } from 'wasi-kernel/src/kernel/services/shared-fs';
 
 import { Pty } from './pty';
+import path from 'path';
 
 
 
@@ -23,7 +24,7 @@ class Shell extends EventEmitter implements ProcessLoader {
 
     constructor() {
         super();
-        this.workerScript = '../node_modules/wasi-kernel/dist/worker.js';
+        this.workerScript = getWorkerUrl();
         this.fgProcesses = [];
         this.pool = new WorkerPool(this.workerScript);
         this.pool.loader = this;
@@ -40,10 +41,16 @@ class Shell extends EventEmitter implements ProcessLoader {
         this.mainProcess = this.spawn('/bin/dash', ['dash'], this.env).process;
     }
 
-    spawn(prog: string, argv: string[], env?: {}) {
+    spawn(prog: string, argv: string[], env?: {[name: string]: string}) {
+        if (!path.isAbsolute(prog) && env.CWD)
+            prog = path.join(env.CWD, prog);
+
         var wasm: string, file = this.files[prog];
-        if (typeof file == 'string' && file.startsWith('#!'))
-            wasm = file.substring(2);
+        if (typeof file == 'string' && file.startsWith('#!')) {
+            let iargs = file.substring(2).split(/\s+/);
+            wasm = iargs[0];
+            argv = [argv[0], ...iargs.slice(1), ...argv.slice(1)];
+        }
         else
             wasm = prog;
 
@@ -74,6 +81,13 @@ class Shell extends EventEmitter implements ProcessLoader {
     }
 }
 
+/**
+ * Note: Parcel updates the 'href' of links during build.
+ */
+function getWorkerUrl() : string {
+    return (<any>document.head.querySelector('link[href^=worker]')).href;
+}
+
 
 
 $(() => {
@@ -93,9 +107,10 @@ $(() => {
     shell.on('data', (x: string) => term.write(x))
     Object.assign(shell.files, {
         '/home/e': require('fs').readFileSync('./bin/example.ml.bin'),
-        '/home/c': require('fs').readFileSync('./bin/ocaml'),
+        '/bin/ocaml.byte': require('fs').readFileSync('./bin/ocaml'),
         '/home/stdlib.cmi': require('fs').readFileSync('./bin/ocaml_stdlib.cmi'),
-        '/bin/ocamlrun': '#!/bin/ocamlrun.wasm'
+        '/bin/ocamlrun': '#!/bin/ocamlrun.wasm',
+        '/bin/ocaml':   '#!/bin/ocamlrun.wasm /bin/ocaml.byte'
     });
     shell.start();
 
